eefe7d90ec8556a7741c01d8966ef1e4cac12973
Sean <ussa.say@gmail.com>
2016-09-11 18:03:36 -0700

    The fun stuff is here!
    The index files have all the fun stuff, actually; dict.js is the boring, yet the most essential part of the entire thing.

---
+++ dict.js
@@ -0,0 +1,10 @@
+/*
+SEAN MADE THIS.
+https://plus.google.com/u/0/+SeanYentheHumansperson
+
+This is where the dictionary is stored.
+*/
+var dict=[
+["you","tu"],
+["exist is am are equals be existence","se",true],
+]
