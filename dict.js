b6249b7aeaf62bfbe453dd7d5f2853f781064eb1
Sean <ussa.say@gmail.com>
2016-09-11 18:59:17 -0700

    More words added
    more coming soon *sighs*

--- dict.js
+++ dict.js
@@ -5,6 +5,32 @@ https://plus.google.com/u/0/+SeanYentheHumansperson
 This is where the dictionary is stored.
 */
 var dict=[
+["is am are equals be","se[noun]"],
+["exist existence","se",true],
+["should, recommend, recommendation, tip","ute[infinitive]",true],
+["warn","inute[infinitive]",true],
+["can ability able","pote[infinitive]",true],
+["must rule guideline required","opore[infinitive]",true],
+["i me we us","ek"], /* pretty sure it wouldn't detect the capital I */
 ["you","tu"],
-["exist is am are equals be existence","se",true],
+["eyo/pronoun it he she him her they them","id"],
+["eyo/properNounIndicator","don"],
+["eyo/abbreviationIndicator","donet"],
+["eyo/pronounVerb","ase"],
+["that","ka"],
+["what","ke"],
+["this","ki"],
+["one thing -body","ko"],
+["all every","ku"],
+["some any","kuo"],
+["zero 0 no without","nin 0"],
+["one 1","un 1"],
+["two 2","dos 2"],
+["three 3","tris 3"],
+["four 4","kat 4"],
+["five 5","kin 5"],
+["six 6","sek 6"],
+["seven 7","sep 7"],
+["eight 8","okt 8"],
+["nine 9","non 9"],
 ]
